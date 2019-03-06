63fe2a4321fe1c7b0d6464247e64522492bfd23e
Ildar Almakaev <ildar-almakayev@mail.ru>
2019-03-06 09:24:06 +0300

    Change initial values for pressure and temperature. Update python file

--- compressible/resonators/axisymmetric/sonicFoam/tkachenko/meta_tkachenko.py
+++ compressible/resonators/axisymmetric/sonicFoam/tkachenko/meta_tkachenko.py
@@ -1,32 +1,34 @@
 import math
 
 L = 1.06
-R = 0.01825
-x0 = 3e-04
+R = 0.012
+x0 = 1.5e-04
 Nx = 100
-Ny = 100
+Ny = 50
 CFL = 0.8
 
-T0 = 296
+T0 = 300
 R_const = 286.8
 gamma = 1.4
 a0 = math.sqrt(gamma * R_const * T0)
-print "Sound speed", a0
+print("Sound speed %f " % a0)
 
-freq = 0.5 * a0 / L
-print "Frequency", freq
+res_freq = 0.5 * a0 / L 
+print("Resonance frequency %f " % res_freq)
+freq = 1.0 * res_freq
+print("Frequency %f " % freq)
 omega = 2 * math.pi * freq
 u0 = omega * x0
-print "U0 ", u0
-print "Cyclic freq omega ", omega
+print("U0 %f " % u0)
+print("Cyclic freq omega %f " % omega)
 period = 1 / freq
-print "Period Tw", period
+print("Period Tw %f " % period)
 
 dx = L / Nx
 dy = R / Ny
 dksi = 1 / (1 / dx + 1 / dy)
 dt = CFL / a0 * dksi
-print "Timestep ", dt
+print("Timestep %.8f " % dt)
 
-print "Write control ", (period / dt / 30)
-print "100 periods ", 100 * period
+print("Write interval to file %f " % (period / dt / 30))
+print("100 periods %f " % (100 * period))
